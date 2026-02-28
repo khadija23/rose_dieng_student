4a3d6d39380b3a34502d294a89b39b9c2780e2c7
boyeazizsamb1010-ai <boyeazizsamb1010@gmail.com>
2026-02-28 15:46:21 +0000

    update script and immage

--- script.js
+++ script.js
@@ -39,14 +39,14 @@ const defaultStudents = [
     },
     {
         id: 6,
-        fullName: "Ousmane Sy",
-        major: "Cybersécurité",
+        fullName: "Abdou Aziz SAMB",
+        major: "Data Scientist and AI",
         imageUrl: "https://images.unsplash.com/photo-1531384441138-2736e62e0919?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-        description: "Avec la digitalisation croissante du continent, sécuriser nos infrastructures numériques est une priorité absolue. L'excellence académique de l'URDFS me prépare à ces défis."
+        description: "J'ai choisi cette formation car je veux aider aux entreprses affricaine de prendre des décisions"
     },
     {
         id: 7,
-        fullName: "Khadija Ba",
+        fullName: "Khadija Ba",défis
         major: "Agro-industrie et Technologies Alimentaires",
         imageUrl: "https://images.unsplash.com/photo-1551836022-d5d88e9218df?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
         description: "L'autosuffisance alimentaire est cruciale. J'étudie comment moderniser nos chaînes de transformation agricole tout en respectant l'environnement et nos traditions."
